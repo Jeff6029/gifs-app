6c69646124331dc146bd8dff7fc416dea31b5007
jeff.dev <johan.06.92@gmail.com>
2025-01-18 16:04:53 +0100

    Proyecto app gifs

---
+++ src/app/gifs/components/search-box/search-box/search-box.component.ts
@@ -0,0 +1,18 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  standalone: false,
+
+  templateUrl: './search-box.component.html',
+  styleUrl: './search-box.component.css'
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag():void {
+    const newTag: string = this.tagInput.nativeElement.value;
+    console.log({newTag});
+  }
+}
---
+++ src/app/shared/components/sidebard/sidebard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  standalone: false,
+
+  templateUrl: './sidebard.component.html',
+  styleUrl: './sidebard.component.css'
+})
+export class SidebardComponent {
+
+}
