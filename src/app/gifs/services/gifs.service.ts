31371c6dde19da03c3011e7ec182d5ba0aa135e9
jeff.dev <johan.06.92@gmail.com>
2025-01-18 20:57:12 +0100

    Realizar peticiones Http usando Angular con una ApiKey de Giphy

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
   private apiKey: string = "lEvlUN6i1Ib643SOIJ5tIARQjXP4yccY";
+  private serviceUrl: string = "https://api.giphy.com/v1/gifs";
   private _tagsHistory: string[] = [];
   // private _tagsHistory: Set<string> = new Set<string>();
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -29,5 +31,15 @@ export class GifsService {
   searchTag(tag: string): void {
     if(tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set("api_key", this.apiKey)
+      .set("limit", "10")
+      .set("q", tag)
+
+    this.http.get(`${this.serviceUrl}/search`, { params })
+      .subscribe( (response: any) => {
+        console.log({response});
+      });
   }
 }
