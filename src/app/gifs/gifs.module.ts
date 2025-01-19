57436d7abcd785eb264f7be92dc76a810dfb1fa3
jeff.dev <johan.06.92@gmail.com>
2025-01-19 01:05:55 +0100

    Crear componente lazy Image

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { HomePageComponent } from './pages/home/home.component';
 import { SearchBoxComponent } from './components/search-box/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
@@ -17,10 +19,10 @@ import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
   ],
   imports: [
     CommonModule,
+    SharedModule
   ],
   exports: [
-    HomePageComponent,
-    CardListComponent
+    HomePageComponent
   ]
 })
 export class GifsModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebardComponent } from './components/sidebard/sidebard.component';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebardComponent
+    SidebardComponent,
+    LazyImageComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    SidebardComponent
+    SidebardComponent,
+    LazyImageComponent
   ]
 })
 export class SharedModule { }
